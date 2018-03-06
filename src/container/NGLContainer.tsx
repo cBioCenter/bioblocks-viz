34b0cf0cda200ce68b74373be3ebe0fa3d8e676c
Drew Diamantoukos <MercifulCode@gmail.com>
2018-03-06 11:59:33 -0500

    Basic rendering of NGL and 3DMol

--- src/container/NGLContainer.tsx
+++ src/container/NGLContainer.tsx
@@ -1,16 +1,23 @@
+import * as NGL from 'ngl';
 import * as React from 'react';
 
-import { NGLComponent } from '../component/NGLComponent';
+let canvas: any;
 
 export class NGLContainer extends React.Component<any, any> {
-  public constructor(props: any) {
+  constructor(props: any) {
     super(props);
+    this.state = { el: undefined };
+  }
+
+  public componentDidMount() {
+    const stage = new NGL.Stage(canvas);
+    stage.loadFile('rcsb://1crn', { defaultRepresentation: true });
   }
 
   public render() {
     return (
-      <div id="ContactContainer">
-        <NGLComponent />
+      <div id="NGLContainer">
+        <div ref={el => (canvas = el)} style={{ height: 200, width: 200 }} />
       </div>
     );
   }
