4724df1f4cc7fe70d244f7f119e0ea84180a720d
Drew Diamantoukos <MercifulCode@gmail.com>
2018-03-09 14:40:28 -0500

    Adding more types for ngl

--- src/container/NGLContainer.tsx
+++ src/container/NGLContainer.tsx
@@ -1,6 +1,7 @@
 import * as NGL from 'ngl';
 import * as React from 'react';
 
+import { IStageLoadFileParams, PickingProxy, Stage, StructureComponent } from 'ngl';
 import { GeneInfo } from '../component/GeneInfo';
 
 export class NGLContainer extends React.Component<any, any> {
@@ -9,17 +10,28 @@ export class NGLContainer extends React.Component<any, any> {
     super(props);
   }
 
-  public componentDidMount() {
+  public async componentDidMount() {
     if (this.canvas) {
       const stage = new NGL.Stage(this.canvas);
-      stage.loadFile('assets/1fqg.pdb', { defaultRepresentation: true });
+      const params: IStageLoadFileParams = {
+        defaultRepresentation: true,
+      };
+
+      const structure = (await stage.loadFile('assets/1fqg.pdb', params)) as StructureComponent;
+
+      stage.mouseControls.add('hoverPick', (aStage: Stage, pickingProxy: PickingProxy) => {
+        if (pickingProxy && (pickingProxy.atom || pickingProxy.bond)) {
+          const atom = pickingProxy.atom || pickingProxy.closestBondAtom;
+          console.log(atom.qualifiedName());
+        }
+      });
     }
   }
 
   public render() {
     return (
       <div id="NGLContainer">
-        <div ref={el => (this.canvas = el)} style={{ height: 200, width: 200 }} />
+        <div ref={el => (this.canvas = el)} style={{ height: 400, width: 400 }} />
         <GeneInfo />
       </div>
     );
