1e4463ac48866a3af8d95277ea97530d17ab2043
Drew Diamantoukos <MercifulCode@gmail.com>
2018-03-22 18:52:34 -0400

    Mocking out css file for jest test

--- src/component/__tests__/ChellSlider.test.tsx
+++ src/component/__tests__/ChellSlider.test.tsx
@@ -7,7 +7,7 @@ import * as renderer from 'react-test-renderer';
 import { ChellSlider } from '../ChellSlider';
 
 describe('ChellSlider', () => {
-  test('Should match existing snapshot when given no data.', () => {
+  test('Should match existing snapshot when given simple data.', () => {
     const chellSliderWrapper = shallow(<ChellSlider defaultValue={10} max={100} min={1} label={'test'} />);
     expect(toJson(chellSliderWrapper)).toMatchSnapshot();
   });
