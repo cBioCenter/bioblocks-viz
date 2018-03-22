63c8d3dfa5564120c1fee564e550d48564b17878
Drew Diamantoukos <MercifulCode@gmail.com>
2018-03-22 18:44:54 -0400

    Adding token ChellSlider test

---
+++ src/component/__tests__/ChellSlider.test.tsx
@@ -0,0 +1,14 @@
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
+
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+
+import { ChellSlider } from '../ChellSlider';
+
+describe('ChellSlider', () => {
+  test('Should match existing snapshot when given no data.', () => {
+    const chellSliderWrapper = shallow(<ChellSlider defaultValue={10} max={100} min={1} label={'test'} />);
+    expect(toJson(chellSliderWrapper)).toMatchSnapshot();
+  });
+});
