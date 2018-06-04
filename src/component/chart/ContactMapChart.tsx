954d2eef42918948ff7282626cbe908a58aa1cfa
Drew Diamantoukos <MercifulCode@gmail.com>
2018-06-04 13:50:09 -0400

    Improving scaling for axis gaps and margins.

--- src/component/chart/ContactMapChart.tsx
+++ src/component/chart/ContactMapChart.tsx
@@ -4,14 +4,21 @@ import { RESIDUE_TYPE } from '../../data/chell-data';
 
 import { generateScatterGLData } from '../../helper/PlotlyHelper';
 import { withDefaultProps } from '../../helper/ReactHelper';
-import PlotlyChart, { IPlotlyData } from './PlotlyChart';
+import PlotlyChart, { defaultPlotlyLayout, IPlotlyData } from './PlotlyChart';
 
 export interface IContactMapChartProps {
   candidateResidues: RESIDUE_TYPE[];
   data: IContactMapChartData[];
   dataTransformFn: (entry: IContactMapChartData, nodeSize: number, mirrorPoints: boolean) => Partial<IPlotlyData>;
   nodeSize: number;
+  heightModifier: number;
   hoveredResidues: RESIDUE_TYPE[];
+  legendModifiers: {
+    y: number;
+  };
+  marginModifiers: {
+    b: number;
+  };
   onClickCallback: (...args: any[]) => void;
   onHoverCallback: (...args: any[]) => void;
   onSelectedCallback: (...args: any[]) => void;
@@ -22,7 +29,14 @@ export interface IContactMapChartProps {
 const defaultContactMapChartProps: Partial<IContactMapChartProps> = {
   candidateResidues: new Array<RESIDUE_TYPE>(),
   dataTransformFn: generateScatterGLData,
+  heightModifier: 0.3,
   hoveredResidues: new Array<RESIDUE_TYPE>(),
+  legendModifiers: {
+    y: -0.1,
+  },
+  marginModifiers: {
+    b: 40,
+  },
   onClickCallback: (...args: any[]) => {
     return;
   },
@@ -53,25 +67,50 @@ class ContactMapChartClass extends React.Component<IContactMapChartProps, any> {
   constructor(props: any) {
     super(props);
   }
+
   public render() {
-    const { candidateResidues, data, dataTransformFn, hoveredResidues, nodeSize, range, ...props } = this.props;
+    const {
+      candidateResidues,
+      data,
+      dataTransformFn,
+      heightModifier,
+      hoveredResidues,
+      legendModifiers,
+      marginModifiers,
+      nodeSize,
+      range,
+      ...props
+    } = this.props;
 
     return (
       <PlotlyChart
         data={data.map(entry => dataTransformFn(entry, nodeSize, true))}
         layout={{
+          height: defaultPlotlyLayout.height! + defaultPlotlyLayout.height! * heightModifier,
           legend: {
             orientation: 'h',
-            yanchor: 'top',
+            y: legendModifiers.y * data.length,
+            yanchor: 'bottom',
+          },
+          margin: {
+            b: data.length * marginModifiers.b,
           },
           showlegend: true,
           xaxis: {
             autorange: false,
+            dtick: 10,
             range,
+            showline: true,
+            tickmode: 'linear',
+            title: 'Residue #',
           },
           yaxis: {
             autorange: false,
+            dtick: 10,
             range: range.slice().reverse(),
+            showline: true,
+            tickmode: 'linear',
+            title: 'Residue #',
           },
         }}
         {...props}
