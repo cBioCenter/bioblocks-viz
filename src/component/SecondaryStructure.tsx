a01d6cca4c6d77a4d48c89deb53dc0d35aac9055
Drew Diamantoukos <MercifulCode@gmail.com>
2018-06-08 11:23:18 -0400

    Added SecondaryStructure enum mapping, font with ligature for component

--- src/component/SecondaryStructure.tsx
+++ src/component/SecondaryStructure.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { SECONDARY_STRUCTURE_CODES } from '../data/chell-data';
 
 export interface ISecondaryStructureProps {
   sequence: string[];
@@ -9,8 +10,26 @@ export default class SecondaryStructure extends React.Component<ISecondaryStruct
     super(props);
   }
 
+  // Points arrow down: transform: 'rotate(90deg)'
   public render() {
-    return <div>{this.props.sequence}</div>;
+    return (
+      <div style={{ fontFamily: 'fira code', fontSize: 16, textAlign: 'right' }}>
+        {this.props.sequence.map(char => this.deriveSecondaryStructureCode(char))}
+      </div>
+    );
+  }
+
+  protected deriveSecondaryStructureCode(code: string) {
+    switch (code) {
+      case SECONDARY_STRUCTURE_CODES.ALPHA_HELIX:
+        return '~';
+      case SECONDARY_STRUCTURE_CODES.BETA_SHEET:
+        return '->';
+      case SECONDARY_STRUCTURE_CODES.COIL:
+        return '-';
+      default:
+        return code;
+    }
   }
 }
 
