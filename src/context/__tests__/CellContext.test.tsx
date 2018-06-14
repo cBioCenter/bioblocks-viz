3821eaa8d79ece8a637ef4b23944d3da43c03b93
Drew Diamantoukos <MercifulCode@gmail.com>
2018-06-14 16:50:37 -0400

    Context tests

--- src/context/__tests__/CellContext.test.tsx
+++ src/context/__tests__/CellContext.test.tsx
@@ -1,15 +1,24 @@
 import * as React from 'react';
 import * as Renderer from 'react-test-renderer';
 
-import CellContext from '../CellContext';
+import CellContext, { initialCellContext } from '../CellContext';
 
 describe('CellContext', () => {
   it('Should match the default snapshot.', () => {
     const wrapper = Renderer.create(
-      <CellContext.Consumer>
-        {({ addCells, currentCells, removeAllCells, removeCells }) => <div />}
-      </CellContext.Consumer>,
+      <CellContext.Consumer>{context => React.createElement('div', context)}</CellContext.Consumer>,
     );
     expect(wrapper.toJSON()).toMatchSnapshot();
+    expect(wrapper.root.props).toEqual(initialCellContext);
+  });
+
+  it('Should have empty initial functions that produce 0 side effects.', () => {
+    const wrapper = Renderer.create(
+      <CellContext.Consumer>{context => React.createElement('div', context)}</CellContext.Consumer>,
+    );
+    wrapper.root.props.addCells();
+    wrapper.root.props.removeAllCells();
+    wrapper.root.props.removeCells();
+    expect(wrapper.root.props).toEqual(initialCellContext);
   });
 });
