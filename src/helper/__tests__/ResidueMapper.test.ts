6e97b0a663c86c1944a684933a0f1f4c3ff9354a
Drew Diamantoukos <MercifulCode@gmail.com>
2018-07-20 14:39:24 -0400

    Extra little test

--- src/helper/__tests__/ResidueMapper.test.ts
+++ src/helper/__tests__/ResidueMapper.test.ts
@@ -22,8 +22,17 @@ describe('ResidueMapper', () => {
     expect(result).toEqual(expected);
   });
 
-  it('Should map residues correctly when given properly formatted indextableplus text.', () => {
-    const result = generateResidueMapping(simpleFile);
+  it('Should throw when given text that is missing the required headers.', () => {
+    expect(() => generateResidueMapping('1\t2\t3\t4\t5')).toThrowError();
+  });
+
+  it('Should return an empty list if headers are correct but no other text is provided.', () => {
+    const expected = new Array();
+    const result = generateResidueMapping(simpleFile.split('\n')[0]);
+    expect(result).toEqual(expected);
+  });
+
+  it('Should skip empty lines.', () => {
     const expected = [
       {
         pdbResname: 'H',
@@ -38,6 +47,12 @@ describe('ResidueMapper', () => {
         uniProtResno: 25,
       },
     ] as IResidueMapping[];
+    const splitLines = simpleFile.split('\n');
+    const foo = splitLines[0]
+      .concat('\n', splitLines[1])
+      .concat('\n', '')
+      .concat('\n', splitLines[2]);
+    const result = generateResidueMapping(foo);
     expect(result).toEqual(expected);
   });
 });
