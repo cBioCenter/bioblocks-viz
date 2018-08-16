c1d6f3c9f70ad8e35b60924061d0bfc6fdea3f45
Drew Diamantoukos <MercifulCode@gmail.com>
2018-08-16 15:06:15 -0400

    Exporting everything for chell-viz library consumption.

--- chell-viz.ts
+++ chell-viz.ts
@@ -1,18 +1,41 @@
-export { default as CategorySelector } from './src/component/CategorySelector';
+export { default as AuxiliaryAxis } from './src/component/chart/AuxiliaryAxis';
+export { default as ContactMapChart } from './src/component/chart/ContactMapChart';
+export { default as PlotlyChart } from './src/component/chart/PlotlyChart';
+export { default as SecondaryStructureAxis } from './src/component/chart/SecondaryStructureAxis';
+
+export { default as ChellRadioGroup } from './src/component/widget/ChellRadioGroup';
 export { default as ChellSlider } from './src/component/widget/ChellSlider';
+export { default as ChellTooltip } from './src/component/widget/ChellTooltip';
+export { default as ChellWidget } from './src/component/widget/ChellWidget';
+
+export { default as CategorySelector } from './src/component/CategorySelector';
 export { default as ContactMap } from './src/component/ContactMap';
+export { default as InfoPanel } from './src/component/InfoPanel';
 export { default as NGLComponent } from './src/component/NGLComponent';
+export { default as PredictedContactMap } from './src/component/PredictedContactMap';
 export { default as SpringComponent } from './src/component/SpringComponent';
 export { default as TComponent } from './src/component/TComponent';
 export { default as VizSelectorPanel } from './src/component/VizSelectorPanel';
+
 export { default as VizPanelContainer } from './src/container/VizPanelContainer';
 
-export {
-  fetchAppropriateData,
-  fetchContactMapData,
-  fetchNGLDataFromDirectory,
-  fetchNGLDataFromFile,
-  getCouplingScoresData,
-} from './src/helper/DataHelper';
+export * from './src/context/CellContext';
+export * from './src/context/ChellContext';
+export * from './src/context/ResidueContext';
+export * from './src/context/SecondaryStructureContext';
+
+export * from './src/data/chell-data';
+export * from './src/data/Chell1DSection';
+export * from './src/data/ChellPDB';
+export * from './src/data/CouplingContainer';
+export * from './src/data/Spring';
+
+export * from './src/helper/DataHelper';
+export * from './src/helper/FetchHelper';
+export * from './src/helper/NGLHelper';
+export * from './src/helper/PlotlyHelper';
+export * from './src/helper/ResidueMapper';
+
+export * from './src/data/event/ChellChartEvent';
 
 export const ANGSTROM_CHAR = String.fromCharCode(0x212b);
