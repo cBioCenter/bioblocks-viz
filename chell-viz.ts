0e4e191630eaeddf10c359a381b7af24cb134663
Drew Diamantoukos <MercifulCode@gmail.com>
2018-10-16 10:39:22 -0400

    Directly importing semantic flat theme for external consumption

--- chell-viz.ts
+++ chell-viz.ts
@@ -1,3 +1,6 @@
 export * from './src';
 
+// tslint:disable-next-line:no-import-side-effect no-relative-imports
+import './assets/semantic.flat.min.css';
+
 export const ANGSTROM_CHAR = String.fromCharCode(0x212b);
