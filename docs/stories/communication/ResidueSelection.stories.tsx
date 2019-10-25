f158f4977f2722d1a13ec5d0283fbd4fc2c67bd8
Drew Diamantoukos <MercifulCode@gmail.com>
2019-10-25 10:14:42 -0400

    Deps update. Having ContactMapContainer properly extend BioblocksVisualization. Factored out part of componentDidUpdate conditional into separate function.

--- docs/stories/communication/ResidueSelection.stories.tsx
+++ docs/stories/communication/ResidueSelection.stories.tsx
@@ -3,15 +3,15 @@ import * as React from 'react';
 
 import { ContactMapContainer, NGLContainer } from '~bioblocks-viz~/container';
 import { BioblocksPDB, CouplingContainer } from '~bioblocks-viz~/data';
+import { fetchContactMapData } from '~bioblocks-viz~/helper';
 
 export default {
   component: [ContactMapContainer, NGLContainer],
-  title: 'communication/Anatomogram',
+  title: 'communication/Residue Selection',
 };
 
 if (window) {
   window.addEventListener('message', e => {
-    console.log('oi');
     console.log(e);
   });
 }
@@ -19,30 +19,60 @@ if (window) {
 export const Foo = () => {
   return (
     <div>
-      <iframe
-        id="bioblocks-frame-2"
-        sandbox={'allow-scripts allow-same-origin'}
-        width="525"
-        height="530"
-        src="bioblocks.html"
-        onLoad={() => {
-          const iframe = document.getElementById('bioblocks-frame-2');
-          // @ts-ignore
-          if (iframe && iframe.contentWindow) {
-            // @ts-ignore
-            // tslint:disable-next-line: no-unsafe-any
-            iframe.contentWindow.postMessage(
-              {
-                props: {
-                  predictedProteins: ['datasets/beta_lactamase/1ZG4.pdb'],
-                },
-                viz: 'NGL',
-              },
-              '*',
-            );
-          }
-        }}
-      />
+      {getFramedNGL()}
+      {getFramedContactMap()}
     </div>
   );
 };
+
+const getFramedNGL = () => (
+  <iframe
+    id="bioblocks-frame-ngl"
+    sandbox={'allow-scripts allow-same-origin'}
+    width="525"
+    height="590"
+    src="bioblocks.html"
+    onLoad={() => {
+      const iframe = document.getElementById('bioblocks-frame-ngl');
+      // @ts-ignore
+      if (iframe && iframe.contentWindow) {
+        // @ts-ignore
+        // tslint:disable-next-line: no-unsafe-any
+        iframe.contentWindow.postMessage(
+          {
+            props: {
+              predictedProteins: ['datasets/beta_lactamase/1ZG4.pdb'],
+            },
+            viz: 'NGL',
+          },
+          '*',
+        );
+      }
+    }}
+  />
+);
+
+const getFramedContactMap = () => (
+  <iframe
+    id="bioblocks-frame-contact-map"
+    sandbox={'allow-scripts allow-same-origin'}
+    width="525"
+    height="590"
+    src="bioblocks.html"
+    onLoad={() => {
+      const iframe = document.getElementById('bioblocks-frame-contact-map');
+      // @ts-ignore
+      if (iframe && iframe.contentWindow) {
+        // @ts-ignore
+        // tslint:disable-next-line: no-unsafe-any
+        iframe.contentWindow.postMessage(
+          {
+            props: {},
+            viz: 'Contact Map',
+          },
+          '*',
+        );
+      }
+    }}
+  />
+);
