3ff36fd93dc3d66b1be7e3ca47fa718bacc8e745
Drew Diamantoukos <MercifulCode@gmail.com>
2020-03-19 11:53:49 -0400

    Added props for camera type and fov.

--- docs/stories/visualization/NGL.stories.tsx
+++ docs/stories/visualization/NGL.stories.tsx
@@ -1,19 +1,24 @@
-import { storiesOf } from '@storybook/react';
 import * as React from 'react';
 
+import { number, select } from '@storybook/addon-knobs';
 import { NGLContainer } from '~bioblocks-viz~/container';
 
-const stories = storiesOf('visualization/NGL', module).addParameters({ component: NGLContainer });
+export default {
+  component: NGLContainer,
+  title: 'visualization/NGL',
+};
 
-stories.add(
-  'Beta Lactamase 1ZG4',
-  () => (
-    <NGLContainer
-      experimentalProteins={['datasets/beta_lactamase/1zg4.pdb', 'datasets/beta_lactamase/1zg4_copy.pdb']}
-      predictedProteins={['datasets/1zg4/1zg4.pdb', 'datasets/1zg4/1zg4_copy.pdb']}
-    />
-  ),
-  {
-    info: { inline: true },
-  },
+export const BetaLactamase1ZG4 = () => (
+  <NGLContainer
+    experimentalProteins={['datasets/beta_lactamase/1zg4.pdb', 'datasets/beta_lactamase/1zg4_copy.pdb']}
+    predictedProteins={['datasets/1zg4/1zg4.pdb', 'datasets/1zg4/1zg4_copy.pdb']}
+  />
+);
+
+export const Camera = () => (
+  <NGLContainer
+    cameraFov={number('Stereo Camera FOV', 65)}
+    cameraType={select('Camera Type', ['perspective', 'orthographic', 'stereo'], 'stereo')}
+    experimentalProteins={['datasets/beta_lactamase/1zg4.pdb']}
+  />
 );
